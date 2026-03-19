a85ad7a02fffa6e8839e6f6dff76d8fe160179ad
WaltDev29 <waltdev29@gmail.com>
2026-03-20 00:04:52 +0900

    # 환경변수로 Tool 선택하도록 구현

--- agent/tools/all_tools.py
+++ agent/tools/all_tools.py
@@ -1,10 +1,14 @@
 from .robot import pybullet_tool
+from .robot import dofbot_tool
 from .vision import yolo_tool
 from .vision import realsense_tool
+from ..core.config import config
 
+tools = []
 
-tools = [
-    *pybullet_tool.tools,
-    *yolo_tool.tools,
-    *realsense_tool.tools
-    ]
+if config.DOFBOT:
+    tools.extend(dofbot_tool.tools)
+    tools.extend(realsense_tool.tools)
+else:
+    tools.extend(pybullet_tool.tools)
+    tools.extend(yolo_tool.tools)
