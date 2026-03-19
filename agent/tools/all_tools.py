935ce3347b571bc5164c25a6dc21d55473db2857
WaltDev29 <waltdev29@gmail.com>
2026-03-19 23:02:45 +0900

    # Realsense Tool 추가 - 각 객체의 중앙 좌표, 중앙 좌표 depth 반환

--- agent/tools/all_tools.py
+++ agent/tools/all_tools.py
@@ -1,10 +1,10 @@
 from .robot import pybullet_tool
 from .vision import yolo_tool
-from .robot import through_tool
+from .vision import realsense_tool
 
 
 tools = [
     *pybullet_tool.tools,
     *yolo_tool.tools,
-    *through_tool.tools
+    *realsense_tool.tools
     ]
